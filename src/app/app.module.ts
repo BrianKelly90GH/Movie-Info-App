0ac96284cce43b36a1495af604c7201c657f65d6
Brian Kelly <briankelly90united@gmail.com>
2018-04-02 13:53:11 +0100

    set up project files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
+//Import the main App component
 import { AppComponent } from './app.component';
 
+//Import animations for angualr material
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { HeaderComponent } from './components/header/header.component';
+
+// importing material components
+import {
+  MatFormFieldModule, MatToolbarModule,
+  MatIconModule, MatButtonModule,
+  MatInputModule, MatStepperModule,
+  MatCardModule, MatTooltipModule,
+  MatTabsModule, MatCheckboxModule,
+  MatChipsModule, MatMenuModule, MatTabNav,
+} from '@angular/material';
+
+import {MatDividerModule} from '@angular/material/divider';
+import { BodyComponent } from './components/body/body.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BodyComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatDividerModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
